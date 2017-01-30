033f36c4b243af47ceca518bf985904f44fcb043
Peter Kim <PeterLKim1337@gmail.com>
2017-01-30 13:58:19 -0800

    Added some inportant attributes to the form markup on the customer/create route

--- resources/views/customer/customer-form.blade.php
+++ resources/views/customer/customer-form.blade.php
@@ -10,48 +10,48 @@
         </div>
 
         <div class="form-group col-xs-offset-3  col-xs-6">
-            <label for="inputClaimNumber">Address 1</label>
-            <input type="text" class="form-control" id="inputClaimNumber" placeholder="Address 1">
+            <label for="inputCustomerAddress1">Address 1</label>
+            <input type="text" class="form-control" id="inputCustomerAddress1" name="address1" placeholder="Address 1">
         </div>
 
         <div class="form-group col-xs-offset-3 col-xs-6">
-            <label for="inputClaimNumber">Address 2</label>
-            <input type="text" class="form-control" id="inputClaimNumber" placeholder="Address 2">
+            <label for="inputCustomerAddress2">Address 2</label>
+            <input type="text" class="form-control" id="inputCustomerAddress2" name="address2" placeholder="Address 2">
         </div>
 
         <div class="form-group col-xs-offset-3 col-xs-2">
-            <label for="inputClaimNumber">City</label>
-            <input type="text" class="form-control" id="inputClaimNumber" placeholder="City">
+            <label for="inputCustomerCity">City</label>
+            <input type="text" class="form-control" id="inputCustomerCity" name="city" placeholder="City">
         </div>
 
         <div class="form-group col-xs-2">
-            <label for="inputClaimNumber">State</label>
-            <input type="text" class="form-control" id="inputClaimNumber" placeholder="e.g. WA">
+            <label for="inputCustomerState">State</label>
+            <input type="text" class="form-control" id="inputCustomerState" name="state" placeholder="e.g. WA">
         </div>
 
         <div class="form-group col-xs-2">
-            <label for="inputClaimNumber">Zip</label>
-            <input type="text" class="form-control" id="inputClaimNumber" placeholder="Zip">
+            <label for="inputCustomerZip">Zip</label>
+            <input type="text" class="form-control" id="inputCustomerZip" name="zip" placeholder="Zip">
         </div>
 
         <div class="form-group col-xs-offset-3 col-xs-2">
-            <label for="inputClaimNumber">Phone</label>
-            <input type="text" class="form-control" id="inputClaimNumber" placeholder="###-###-####">
+            <label for="inputCustomerPhone">Phone</label>
+            <input type="text" class="form-control" id="inputCustomerPhone" name="phone" placeholder="###-###-####">
         </div>
 
         <div class="form-group col-xs-2">
-            <label for="inputClaimNumber">Extension</label>
-            <input type="text" class="form-control" id="inputClaimNumber" placeholder="Ext.">
+            <label for="inputCustomerExt">Extension</label>
+            <input type="text" class="form-control" id="inputCustomerExt" name="ext" placeholder="Ext.">
         </div>
 
         <div class="form-group col-xs-2">
-            <label for="inputClaimNumber">Email</label>
-            <input type="text" class="form-control" id="inputClaimNumber" placeholder="example@example.com">
+            <label for="inputCustomerEmail">Email</label>
+            <input type="text" class="form-control" id="inputCustomerEmail" name="email" placeholder="example@example.com">
         </div>
 
         <div class="form-group col-xs-offset-3 col-xs-6">
-            <label for="inputClaimNumber">Comments</label>
-            <textarea class="col-xs-12">Other information that is important to know.</textarea>
+            <label for="inputCustomerComment">Comments</label>
+            <textarea class="col-xs-12" id="inputCustomerComment" name="comments">Other information that is important to know.</textarea>
         </div>
 
         <input class="btn btn-primary col-xs-offset-3 col-xs-6" type="submit" name="submit" value="Submit">
