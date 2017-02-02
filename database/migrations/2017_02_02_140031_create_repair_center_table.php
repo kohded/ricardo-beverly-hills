b1c4c34cdfc24176c405b4ca119724f94f99a1b2
Chris Knoll <cknoll3@gmail.com>
2017-02-02 06:03:08 -0800

    Added migration for Repair Center table.

---
+++ database/migrations/2017_02_02_140031_create_repair_center_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepairCenterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('repair_center', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 40);
+            $table->string('address', 60);
+            $table->string('city', 30);
+            $table->string('state', 2);
+            $table->string('zip', 5);
+            $table->string('phone', 10);
+            $table->string('email', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('repair_center');
+    }
+}
