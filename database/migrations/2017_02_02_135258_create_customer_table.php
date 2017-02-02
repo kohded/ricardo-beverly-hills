70e46e0901049354a1fa026f96df06a9fc0ae04a
Chris Knoll <cknoll3@gmail.com>
2017-02-02 05:59:20 -0800

    Added migration for Customer table.

---
+++ database/migrations/2017_02_02_135258_create_customer_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('claim_id');
+            $table->string('first_name', 30);
+            $table->string('last_name', 30);
+            $table->string('address', 60);
+            $table->string('address_2', 60);
+            $table->string('city', 30);
+            $table->string('state', 2);
+            $table->string('zip', 5);
+            $table->string('phone', 10);
+            $table->string('email', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
