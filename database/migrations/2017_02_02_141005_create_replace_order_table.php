269d0d8c0b57fbb3b561ca2d4d73abafe82ef375
Chris Knoll <cknoll3@gmail.com>
2017-02-02 06:10:50 -0800

    Added migration for Replace Order table.

---
+++ database/migrations/2017_02_02_141005_create_replace_order_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReplaceOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replace_order', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->date('date_shipped');
+            $table->unsignedInteger('customer_id');
+            $table->string('product_style', 11);
+            $table->unsignedInteger('repair_center_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replace_order');
+    }
+}
