277f03ac5c9af5b96f101e39911c068efdb2197b
Chris Knoll <cknoll3@gmail.com>
2017-02-02 07:48:16 -0800

    Added seeding for Repair Center and Product tables.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,7 +23,6 @@ class DatabaseSeeder extends Seeder
     		'password' => '$2y$10$qeSkJ/ubMrXtBn1vbGX9Fusf9R9O83.aRu813lbiPMFt4ZynBkDJK'
     	]);
 
-
         // Seed Customer Table
         foreach (range(1,50) as $index) {
         	DB::table('customer')->insert([
@@ -38,5 +37,32 @@ class DatabaseSeeder extends Seeder
         		'email'      => $faker->email
         	]);
         }
+
+        // Seed Repair Center Table
+        foreach (range(1,15) as $index) {
+	    	DB::table('repair_center')->insert([
+	    		'name'		 => $faker->lastName . ' ' . $faker->city,
+	    		'address'    => $faker->streetAddress,
+	    		'city'       => $faker->city,
+	    		'state'      => $faker->stateAbbr,
+	    		'zip'        => substr($faker->postcode, 0, 5),
+	    		'phone'      => $faker->numberBetween($min = 1000000000, $max = 9999999999),
+	    		'email'      => $faker->email
+	    	]);
+    	}
+
+    	// Seed Product Table
+        foreach (range(1,50) as $index) {
+	    	DB::table('product')->insert([
+	    		'style'		 		=> $faker->regexify('[A-Z0-9]{11}'),
+	    		'description'    	=> $faker->sentence($nbWords = 6,$variableNbWords = true),
+	    		'brand'       		=> 'Ricardo Beverly Hills',
+	    		'warranty_years'    => $faker->randomDigit,
+	    		'color'        		=> $faker->word,
+	    		'class'      		=> $faker->regexify('[0-9]{3}'),
+	    		'class_description' => $faker->sentence($nbWords = 6,$variableNbWords = true),
+	    		'launch_date'		=> $faker->date($format = 'Y-m-d', $max = 'now')
+	    	]);
+    	}   	
     }
 }
