eeb2aea2780bad3b3cce15fa5bda530414657e0e
Chris Knoll <cknoll3@gmail.com>
2017-02-02 06:14:31 -0800

    Made date_shipped field nullable in Replace Order table.

--- database/migrations/2017_02_02_141005_create_replace_order_table.php
+++ database/migrations/2017_02_02_141005_create_replace_order_table.php
@@ -16,7 +16,7 @@ class CreateReplaceOrderTable extends Migration
         Schema::create('replace_order', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
-            $table->date('date_shipped');
+            $table->date('date_shipped')->nullable();
             $table->unsignedInteger('customer_id');
             $table->string('product_style', 11);
             $table->unsignedInteger('repair_center_id');
