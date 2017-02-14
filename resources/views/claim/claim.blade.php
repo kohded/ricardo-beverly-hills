d4bebe3397a803105fe65ecf533c2f02cedc62d1
Chris Knoll <cknoll3@gmail.com>
2017-02-14 13:37:48 -0800

    Restyled claim details into panels and added an Add Comment form.

--- resources/views/claim/claim.blade.php
+++ resources/views/claim/claim.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-    <div id="claim-detail">
+    <div id="claim-detail" class="col-md-8 col-md-offset-2">
         {{--Claim--}}
         <div class="row">
             <div class="col-xs-12">
@@ -19,105 +19,107 @@
                 <p><strong>Opened Date: </strong>{{ $claim[0]->claim_created_at }}</p>
             </div>
             <div class="col-sm-6">
-                <p><strong>Closed Closed: </strong>{{ $claim[0]->claim_date_closed }}</p>
+                <p><strong>Closed Date: </strong>{{ $claim[0]->claim_date_closed }}</p>
             </div>
         </div>
 
-        Product
         <div class="row">
-            <div class="col-xs-12">
-                <h3>Product</h3>
-                <hr>
-            </div>
-            <div class="col-sm-6">
-                <p><strong>Product Style: </strong>{{ $claim[0]->product_style }}</p>
-            </div>
-            <div class="col-sm-6">
-                <p><strong>Damage Code: </strong>{{ $claim[0]->dc_id }}</p>
-            </div>
-        </div>
-
-        Customer
-        <div class="row">
-            <div class="col-xs-12">
-                <h3>Customer</h3>
-                <hr>
-            </div>
-            <div class="col-sm-6">
-                <p><strong>First Name: </strong>{{ $claim[0]->cust_first_name }}</p>
-                <p><strong>Last Name: </strong>{{ $claim[0]->cust_last_name }}</p>
-                <p><strong>Phone: </strong>{{ $claim[0]->cust_phone }}</p>
-                <p><strong>Email: </strong>{{ $claim[0]->cust_email }}</p>
-            </div>
-            <div class="col-sm-6">
-                <p><strong>Address: </strong>{{ $claim[0]->cust_address }}</p>
-                <p><strong>Address 2: </strong>{{ $claim[0]->cust_address_2 }}</p>
-                <p><strong>City: </strong>{{ $claim[0]->cust_city }}</p>
-                <p><strong>State: </strong>{{ $claim[0]->cust_state }}</p>
-                <p><strong>Zip: </strong>{{ $claim[0]->cust_zip }}</p>
-            </div>
-        </div>
-
-        Repair Center
-        <div class="row">
-            <div class="col-xs-12">
-                <h3>Repair Center</h3>
-                <hr>
-            </div>
-            <div class="col-sm-6">
-                <p><strong>Name: </strong>{{ $claim[0]->rc_name }}</p>
-                <p><strong>Contact Name: </strong>{{ $claim[0]->rc_contact }}</p>
-                <p><strong>Phone: </strong>{{ $claim[0]->rc_phone }}</p>
-                <p><strong>Email: </strong>{{ $claim[0]->rc_email }}</p>
-            </div>
-            <div class="col-sm-6">
-                <p><strong>Address: </strong>{{ $claim[0]->rc_address }}</p>
-                <p><strong>City: </strong>{{ $claim[0]->rc_city }}</p>
-                <p><strong>State: </strong>{{ $claim[0]->rc_state }}</p>
-                <p><strong>Zip: </strong>{{ $claim[0]->rc_zip }}</p>
-            </div>
-        </div>
-
-        Comments
-        <div class="row">
-            <div class="col-xs-12">
-                <h3>Comments</h3>
-                <hr>
-            </div>
-
-            @foreach ($comments as $comment)
-                <div class="col-xs-6">
-                    <p><strong>Name: </strong>{{ $comment->author }}</p>
+            <div class="panel panel-info">
+                <div class="panel-heading">Repair Center</div>
+                <div class="panel-body">
+                    <div class="col-sm-6">
+                        <p><strong>Product Style: </strong>{{ $claim[0]->product_style }}</p>
+                    </div>
+                    <div class="col-sm-6">
+                        <p><strong>Damage Code: </strong>{{ $claim[0]->dc_id }}</p>
+                    </div>
                 </div>
-                <div class="col-xs-6">
-                    <p><strong>Created: </strong>{{ $comment->created_at }}</p>
-                </div>
-                <div class="col-xs-12">
-                    <p><strong>Comment: </strong>{{ $comment->comment }}</p>
-                    <hr/>
-                </div>
-            @endforeach
+            </div>
         </div>
 
         <div class="row">
-            Back Button
+            <div class="panel panel-info">
+                <div class="panel-heading">Customer</div>
+                <div class="panel-body">
+                    <div class="col-sm-6">
+                        <p><strong>First Name: </strong>{{ $claim[0]->cust_first_name }}</p>
+                        <p><strong>Last Name: </strong>{{ $claim[0]->cust_last_name }}</p>
+                        <p><strong>Phone: </strong>{{ $claim[0]->cust_phone }}</p>
+                        <p><strong>Email: </strong>{{ $claim[0]->cust_email }}</p>
+                    </div>
+                    <div class="col-sm-6">
+                        <p><strong>Address: </strong>{{ $claim[0]->cust_address }}</p>
+                        <p><strong>Address 2: </strong>{{ $claim[0]->cust_address_2 }}</p>
+                        <p><strong>City: </strong>{{ $claim[0]->cust_city }}</p>
+                        <p><strong>State: </strong>{{ $claim[0]->cust_state }}</p>
+                        <p><strong>Zip: </strong>{{ $claim[0]->cust_zip }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="panel panel-info">
+                <div class="panel-heading">Repair Center</div>
+                <div class="panel-body">
+                    <div class="col-sm-6">
+                        <p><strong>Name: </strong>{{ $claim[0]->rc_name }}</p>
+                        <p><strong>Contact Name: </strong>{{ $claim[0]->rc_contact }}</p>
+                        <p><strong>Phone: </strong>{{ $claim[0]->rc_phone }}</p>
+                        <p><strong>Email: </strong>{{ $claim[0]->rc_email }}</p>
+                    </div>
+                    <div class="col-sm-6">
+                        <p><strong>Address: </strong>{{ $claim[0]->rc_address }}</p>
+                        <p><strong>City: </strong>{{ $claim[0]->rc_city }}</p>
+                        <p><strong>State: </strong>{{ $claim[0]->rc_state }}</p>
+                        <p><strong>Zip: </strong>{{ $claim[0]->rc_zip }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="panel panel-info">
+                <div class="panel-heading">Comments</div>
+
+                <table class="table table-conensed">
+                @foreach ($comments as $comment)
+                    <tr>
+                        <td><strong>Name: </strong>{{ $comment->author }}</td>
+                        <td><strong>Posted: </strong>{{ $comment->created_at }}</td>
+                        <td><strong>Comment: </strong>{{ $comment->comment }}</td>
+                    </tr>
+                @endforeach
+                    <tr><td colspan="3">
+                        <form action="" method="POST">
+                            <div class="form-group col-xs-9">
+                                <input type="text" class="form-control" id="customer-last-name" name="lastname" placeholder="Enter new comment...">
+                            </div>
+                            <div class="form-group col-xs-3">
+                                <button class="btn btn-primary" type="submit">Add Comment</button>
+                            </div>
+                            {{ csrf_field() }}
+                        </form>
+                    </td></tr>
+                </table>
+            </div>
+        </div>
+
+        <div class="row">
             <div class="col-xs-4">
                 <a href="{{ route('claim-index') }}" class="btn btn-primary">
                     Back
                 </a>
             </div>
 
-            Email Button
             <div class="col-xs-8">
                 <form action="{{ route('mail.claim-confirmation') }}"
                       method="post" class="pull-right">
-                    Claim Id
                     <input type="number" name="claim-id" value="{{ $claim[0]->claim_id }}" hidden>
                     <button type="submit" class="btn btn-primary">Send Email</button>
                     {{ csrf_field() }}
                 </form>
 
-                Emails Sent
                 <p class="pull-right mt-10 mr-20"><strong>Emails Sent: </strong>0</p>
             </div>
         </div>
