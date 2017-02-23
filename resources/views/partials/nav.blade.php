6be2616c37f3d38d48ea5460e6b783b28203068c
Arnold Koh <kohded@users.noreply.github.com>
2017-02-23 03:31:08 -0800

    Add roles to nav

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -21,22 +21,37 @@
                 @if (Auth::guest())
                     <li><a href="{{ url('/login') }}">Login</a></li>
                 @else
+                    @role('ricardo-beverly-hills')
                     <li><a href="{{ route('claim-index') }}">Claim</a></li>
                     <li><a href="{{ route('customer') }}">Customer</a></li>
                     <li><a href="{{ route('product') }}">Product</a></li>
                     <li><a href="{{ route('repair-center') }}">Repair Center</a>
+                    @endrole
+
+                    @role('part-company')
+                    <li><a href="{{ route('part-company-claim') }}">Claim</a></li>
+                    @endrole
+
+                    @role('repair-center')
+                    <li><a href="{{ route('repair-center-claim') }}">Claim</a></li>
+                    @endrole
+
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"
                            role="button" aria-expanded="false">
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
+
                         <ul class="dropdown-menu" role="menu">
+                            @role('ricardo-beverly-hills')
                             <li>
                                 <a href="{{ route('dashboard') }}">Dashboard</a>
                             </li>
                             <li>
                                 <a href="{{ url('/register') }}">Register User</a>
                             </li>
+                            @endrole
+
                             <li>
                                 <a href="{{ url('/logout') }}" onclick="event.preventDefault();
                                 document.getElementById('logout-form').submit();">
