126e28f2b4248f6e07d89a3990ab7d14094d41b5
Arnold Koh <kohded@users.noreply.github.com>
2017-04-17 20:05:48 -0700

    Add hr to edit repair center and edit damage code

--- resources/views/damage-code/edit.blade.php
+++ resources/views/damage-code/edit.blade.php
@@ -12,26 +12,26 @@
         <div class="row">
             <form action="{{ route('damage-code.edit-post') }}" method="post">
                 @foreach ($damageCode as $info)
-                {{--Name--}}
-                <div class="form-group col-sm-4">
-                    <label for="damage-code-new-id">Damage Code ID</label>
-                    <input type="text" class="form-control"
-                           id="damage-code-new-id" name="newId"
-                           value="{{ $info->id }}">
-                </div>
-                {{--Contact Name--}}
-                <div class="form-group col-sm-8">
-                    <label for="damage-code-part">Part/Description</label>
-                    <input type="text" class="form-control"
-                           id="damage-code-part" name="part"
-                           value="{{ $info->part }}">
-                </div>
-
-                {{--Id--}}
-                <input type="hidden" name="id" value="{{ $info->id }}">
+                    {{--Damage Code ID--}}
+                    <div class="form-group col-sm-4">
+                        <label for="damage-code-new-id">Damage Code ID</label>
+                        <input type="text" class="form-control"
+                               id="damage-code-new-id" name="newId"
+                               value="{{ $info->id }}">
+                    </div>
+                    {{--Part/Description--}}
+                    <div class="form-group col-sm-8">
+                        <label for="damage-code-part">Part/Description</label>
+                        <input type="text" class="form-control"
+                               id="damage-code-part" name="part"
+                               value="{{ $info->part }}">
+                    </div>
+                    {{--Id--}}
+                    <input type="hidden" name="id" value="{{ $info->id }}">
                 @endforeach
                 {{--Submit--}}
                 <div class="form-group col-xs-12">
+                    <hr>
                     <a href="{{ route('damage-code') }}" class="btn btn-primary">
                         Back
                     </a>
@@ -54,6 +54,7 @@
                 </div>
             </div>
         @endif
+
         {{--Form validation errors--}}
         @if(count($errors) > 0)
             <div class="row">
--- resources/views/repair-center/edit.blade.php
+++ resources/views/repair-center/edit.blade.php
@@ -12,76 +12,78 @@
         <div class="row">
             <form action="{{ route('repair-center.edit-post') }}" method="post">
                 @foreach ($repairCenter as $info)
-                {{--Name--}}
-                <div class="form-group col-sm-6">
-                    <label for="repair-center-name">Name</label>
-                    <input type="text" class="form-control"
-                           id="repair-center-name" name="name"
-                           value="{{ $info->name }}">
-                </div>
-                {{--Contact Name--}}
-                <div class="form-group col-sm-6">
-                    <label for="repair-center-contact-name">Contact Name</label>
-                    <input type="text" class="form-control"
-                           id="repair-center-contact-name" name="contact-name"
-                           value="{{ $info->contact_name }}">
-                </div>
-                {{--Phone--}}
-                <div class="form-group col-sm-6">
-                    <label for="repair-center-phone">Phone #</label>
-                    <input type="tel" class="form-control"
-                           id="repair-center-phone" name="phone"
-                           value="{{ $info->phone }}">
-                </div>
-                {{--Email--}}
-                <div class="form-group col-sm-6">
-                    <label for="repair-center-email">Email</label>
-                    <input type="email" class="form-control"
-                           id="repair-center-email" name="email"
-                           value="{{ $info->email }}">
-                </div>
-                {{--Address--}}
-                <div class="form-group col-xs-12">
-                    <label for="repair-center-address">Address</label>
-                    <input type="text" class="form-control"
-                           id="repair-center-address" name="address"
-                           value="{{ $info->address }}">
-                </div>
-                {{--City--}}
-                <div class="form-group col-sm-6">
-                    <label for="repair-center-city">City</label>
-                    <input type="text" class="form-control"
-                           id="repair-center-city" name="city"
-                           value="{{ $info->city }}">
-                </div>
-                {{--State--}}
-                <div class="form-group col-xs-6 col-sm-3">
-                    <label for="repair-center-state">State</label>
-                    <input type="text" class="form-control"
-                           id="repair-center-state" name="state"
-                           value="{{ $info->state }}">
-                </div>
-                {{--Zip--}}
-                <div class="form-group col-xs-6 col-sm-3">
-                    <label for="repair-center-zip">Zip</label>
-                    <input type="text" class="form-control"
-                           id="repair-center-zip" name="zip"
-                           value="{{ $info->zip }}">
-                </div>
-                {{--Preferred--}}
-                <div class="form-check col-xs-6 col-xs-offset-6">
-                    <label for="repair-center-preferred" class="pull-right" id="repair-center-preferred-label">
-                        <input type="checkbox" class="form-check-input"
-                               id="repair-center-preferred" name="preferred"
-                               value="1" {{ $info->preferred ? 'checked' : '' }}>
-                        Preferred
-                    </label>
-                </div>
-                {{--Id--}}
-                <input type="hidden" name="id" value="{{ $info->id }}">
+                    {{--Name--}}
+                    <div class="form-group col-sm-6">
+                        <label for="repair-center-name">Name</label>
+                        <input type="text" class="form-control"
+                               id="repair-center-name" name="name"
+                               value="{{ $info->name }}">
+                    </div>
+                    {{--Contact Name--}}
+                    <div class="form-group col-sm-6">
+                        <label for="repair-center-contact-name">Contact Name</label>
+                        <input type="text" class="form-control"
+                               id="repair-center-contact-name" name="contact-name"
+                               value="{{ $info->contact_name }}">
+                    </div>
+                    {{--Phone--}}
+                    <div class="form-group col-sm-6">
+                        <label for="repair-center-phone">Phone #</label>
+                        <input type="tel" class="form-control"
+                               id="repair-center-phone" name="phone"
+                               value="{{ $info->phone }}">
+                    </div>
+                    {{--Email--}}
+                    <div class="form-group col-sm-6">
+                        <label for="repair-center-email">Email</label>
+                        <input type="email" class="form-control"
+                               id="repair-center-email" name="email"
+                               value="{{ $info->email }}">
+                    </div>
+                    {{--Address--}}
+                    <div class="form-group col-xs-12">
+                        <label for="repair-center-address">Address</label>
+                        <input type="text" class="form-control"
+                               id="repair-center-address" name="address"
+                               value="{{ $info->address }}">
+                    </div>
+                    {{--City--}}
+                    <div class="form-group col-sm-6">
+                        <label for="repair-center-city">City</label>
+                        <input type="text" class="form-control"
+                               id="repair-center-city" name="city"
+                               value="{{ $info->city }}">
+                    </div>
+                    {{--State--}}
+                    <div class="form-group col-xs-6 col-sm-3">
+                        <label for="repair-center-state">State</label>
+                        <input type="text" class="form-control"
+                               id="repair-center-state" name="state"
+                               value="{{ $info->state }}">
+                    </div>
+                    {{--Zip--}}
+                    <div class="form-group col-xs-6 col-sm-3">
+                        <label for="repair-center-zip">Zip</label>
+                        <input type="text" class="form-control"
+                               id="repair-center-zip" name="zip"
+                               value="{{ $info->zip }}">
+                    </div>
+                    {{--Preferred--}}
+                    <div class="form-check col-xs-6 col-xs-offset-6">
+                        <label for="repair-center-preferred" class="pull-right"
+                               id="repair-center-preferred-label">
+                            <input type="checkbox" class="form-check-input"
+                                   id="repair-center-preferred" name="preferred"
+                                   value="1" {{ $info->preferred ? 'checked' : '' }}>
+                            Preferred
+                        </label>
+                    </div>
+                    {{--Id--}}
+                    <input type="hidden" name="id" value="{{ $info->id }}">
                 @endforeach
                 {{--Submit--}}
                 <div class="form-group col-xs-12">
+                    <hr>
                     <a href="{{ route('repair-center') }}" class="btn btn-primary">
                         Back
                     </a>
@@ -104,6 +106,7 @@
                 </div>
             </div>
         @endif
+
         {{--Form validation errors--}}
         @if(count($errors) > 0)
             <div class="row">
