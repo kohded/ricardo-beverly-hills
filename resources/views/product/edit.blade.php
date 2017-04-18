ffaa5c8c3c2c9927e249103af25802db9aa70097
Arnold Koh <kohded@users.noreply.github.com>
2017-04-17 20:01:26 -0700

    Update edit product with narrow master layout

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -1,68 +1,78 @@
-@extends('layouts.master')
+@extends('layouts.master-narrow')
 
 @section('content')
-    <div id="product-create">
+    <div id="product-edit">
         <div class="row">
-            <div class="col-xs-12 col-md-6 col-md-offset-3">
-                {{--<h4>{{ $title }}</h4>--}}
+            <div class="col-xs-12">
+                <h2>Edit Product</h2>
+                <hr>
+            </div>
+        </div>
 
-                <form action="{{ route('product.edit-post') }}" method="post">
-                    @foreach ($product as $info)
-                    <legend>Edit Product</legend>
-
-                    <div class="form-group col-xs-6">
-                        <label for="product-style">Style #</label>
-                        <input type="text" class="form-control" id="product-style" name="style" placeholder="" value="{{ $info->style }}">
-                    </div>
-
-                    <div class="form-group col-xs-6">
-                        <label for="product-description">Description</label>
-                        <input type="text" class="form-control" id="product-description" name="description" placeholder="" value="{{ $info->description }}">
-                    </div>
-
-                    <div class="form-group col-xs-6">
+        <div class="row">
+            <form action="{{ route('product.edit-post') }}" method="post">
+                @foreach ($product as $info)
+                    {{--Brand--}}
+                    <div class="form-group col-sm-6">
                         <label for="product-brand">Brand</label>
                         <input type="text" class="form-control"
-                               id="product-brand" name="brand" placeholder="" value="{{ $info->brand }}">
+                               id="product-brand" name="brand" placeholder=""
+                               value="{{ $info->brand }}">
                     </div>
-
-                    <div class="form-group col-xs-3">
-                        <label for="product-warranty-years">Warranty</label>
-                        <input type="text" class="form-control" id="product-warranty-years" name="warranty" placeholder="" value="{{ $info->warranty_years }}">
-                    </div>
-
-                    <div class="form-group col-xs-3">
-                        <label for="product-color">Color</label>
-                        <input type="text" class="form-control" id="product-color" name="color" placeholder="Color"  value="{{ $info->color }}">
-                    </div>
-
-                    <div class="form-group col-xs-6">
+                    {{--Collection--}}
+                    <div class="form-group col-sm-6">
                         <label for="product-class">Collection</label>
-                        <input type="text" class="form-control" id="product-collection" name="collection" placeholder="Class" value="{{ $info->collection }}">
+                        <input type="text" class="form-control" id="product-collection"
+                               name="collection" placeholder="Class"
+                               value="{{ $info->collection }}">
                     </div>
-
-                    <div class="form-group col-xs-4 col-xs-offset-2">
+                    {{--Style--}}
+                    <div class="form-group col-xs-6">
+                        <label for="product-style">Style #</label>
+                        <input type="text" class="form-control" id="product-style" name="style"
+                               placeholder="" value="{{ $info->style }}">
+                    </div>
+                    {{--Color--}}
+                    <div class="form-group col-xs-6">
+                        <label for="product-color">Color</label>
+                        <input type="text" class="form-control" id="product-color" name="color"
+                               placeholder="Color" value="{{ $info->color }}">
+                    </div>
+                    {{--Launch Date--}}
+                    <div class="form-group col-xs-6">
                         <label for="product-launch-date">Launch Date</label>
-                        <input type="text" class="form-control" id="product-launch-date" name="launch" placeholder="mm/dd/yyyy" value="{{ $info->launch_date }}">
+                        <input type="text" class="form-control" id="product-launch-date"
+                               name="launch" placeholder="mm/dd/yyyy"
+                               value="{{ $info->launch_date }}">
                     </div>
-
-                    @endforeach
-
+                    {{--Warranty--}}
+                    <div class="form-group col-xs-6">
+                        <label for="product-warranty-years">Warranty</label>
+                        <input type="text" class="form-control" id="product-warranty-years"
+                               name="warranty" placeholder="" value="{{ $info->warranty_years }}">
+                    </div>
+                    {{--Description--}}
                     <div class="form-group col-xs-12">
-                        <hr />
-                        <a href="{{ route('product') }}" class="btn btn-primary">Back</a>
-                        <button type="submit" class="btn btn-primary pull-right">Submit</button>
+                        <label for="product-description">Description</label>
+                        <input type="text" class="form-control" id="product-description"
+                               name="description" placeholder="" value="{{ $info->description }}">
                     </div>
-                    {{--Token--}}
-                    {{ csrf_field() }}
-                </form>
-            </div>
+                @endforeach
+                {{--Submit--}}
+                <div class="form-group col-xs-12">
+                    <hr>
+                    <a href="{{ route('product') }}" class="btn btn-primary">Back</a>
+                    <button type="submit" class="btn btn-primary pull-right">Submit</button>
+                </div>
+                {{--Token--}}
+                {{ csrf_field() }}
+            </form>
         </div>
 
         {{--Form successfully added product--}}
         @if(Session::has('message'))
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-xs-12">
                     <p class="alert alert-success">
                         {{ Session::get('message') }}
                     </p>
@@ -73,7 +83,7 @@
         {{--Form validation errors--}}
         @if(count($errors) > 0)
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-xs-12">
                     <div class="alert alert-danger">
                         <ul>
                             @foreach ($errors->all() as $error)
@@ -84,6 +94,5 @@
                 </div>
             </div>
         @endif
-        
     </div>
 @endsection
