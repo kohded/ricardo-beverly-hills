010bc0c71233f8c8cdc56197a83866e090dda97a
Arnold Koh <kohded@users.noreply.github.com>
2017-04-17 21:30:44 -0700

    Update create damage code with narrow master layout

--- resources/views/damage-code/create.blade.php
+++ resources/views/damage-code/create.blade.php
@@ -1,32 +1,33 @@
-@extends('layouts.master')
+@extends('layouts.master-narrow')
 
 @section('content')
     <div id="damage-code-create">
         <div class="row">
             <div class="col-xs-12">
-                <h1>Create Damage Code</h1>
+                <h2>Create Damage Code</h2>
+                <hr>
             </div>
         </div>
 
         <div class="row">
             <form action="{{ route('damage-code.create') }}" method="post">
-                {{--Name--}}
-                <div class="form-group col-sm-4">
-                    <label for="damage-code-name">Damage Code ID</label>
+                {{--Damage Code ID--}}
+                <div class="form-group col-xs-4">
+                    <label for="damage-code-name">Code</label>
                     <input type="text" class="form-control"
                            id="damage-code-id" name="id"
                            value="{{ old('id') }}">
                 </div>
-                {{--Contact Name--}}
-                <div class="form-group col-sm-8">
+                {{--Part/Description--}}
+                <div class="form-group col-xs-8">
                     <label for="damage-code-part">Part/Description</label>
                     <input type="text" class="form-control"
                            id="damage-code-part" name="part"
                            value="{{ old('part') }}">
                 </div>
-
                 {{--Submit--}}
                 <div class="form-group col-xs-12">
+                    <hr>
                     <a href="{{ route('damage-code') }}" class="btn btn-primary">
                         Back
                     </a>
@@ -42,17 +43,18 @@
         {{--Form successfully added damage code--}}
         @if(Session::has('message'))
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-xs-12">
                     <p class="alert alert-success">
                         {{ Session::get('message') }}
                     </p>
                 </div>
             </div>
         @endif
+
         {{--Form validation errors--}}
         @if(count($errors) > 0)
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-xs-12">
                     <div class="alert alert-danger">
                         <ul>
                             @foreach ($errors->all() as $error)
