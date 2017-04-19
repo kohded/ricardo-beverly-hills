4947e8936380133a3ab1f1f41c60158804851e98
Chris Knoll <cknoll3@gmail.com>
2017-04-19 13:56:18 -0700

    Typing in a route that doesn't exist now redirects to root.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,7 +44,29 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if($this->isHttpException($exception))
+        {
+            switch ($exception->getStatusCode()) 
+                {
+                // not found
+                case '404':
+                return redirect('/');
+                break;
+
+                // internal error
+                case '500':
+                return redirect('/');
+                break;
+
+                default:
+                    return $this->renderHttpException($exception);
+                break;
+            }
+        }
+        else
+        {
+            return parent::render($request, $exception);
+        }
     }
 
     /**
