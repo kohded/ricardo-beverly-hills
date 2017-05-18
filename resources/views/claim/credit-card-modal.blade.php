735c4bc2eec55b2c21cd561e946f4dc57f8d704e
Chris Knoll <cknoll3@gmail.com>
2017-05-18 09:25:26 -0700

    Added form to credit card modal

--- resources/views/claim/credit-card-modal.blade.php
+++ resources/views/claim/credit-card-modal.blade.php
@@ -11,10 +11,65 @@
           Credit Card Information Capture / Print
         </h4>
       </div>
-      <div class="modal-body">
+      <div class="modal-body col-sm-12">
         <p>
-          This is where the user will be able to enter credit card information and print.  It won't be saved to a database or processed by the server due to security reasons.
+          <span class="fa fa-exclamation-triangle text-danger" aria-hidden="true"></span>
+          <strong>This information MUST be printed, it will be erased once this modal closes.</strong>
         </p>
+        {{--Billing Name--}}
+        <div class="form-group col-sm-12">
+            <label for="ccname">Name (Exactly as it appears on card)</label>
+            <input type="text" class="form-control" id="customer-first-name"
+                   name="ccname">
+        </div>
+        {{--CC Number--}}
+        <div class="form-group col-sm-12">
+            <label for="ccname">Credit Card Number</label>
+            <input type="text" class="form-control" id="customer-first-name"
+                   name="ccname">
+        </div>
+        {{--Expiration--}}
+        <div class="form-group col-sm-3">
+            <label for="ccname">Expiration</label>
+            <input type="text" class="form-control" id="customer-first-name"
+                   name="ccname" placeholder="##/##">
+        </div>
+        {{--Security Code--}}
+        <div class="form-group col-sm-3">
+            <label for="ccname">Security Code</label>
+            <input type="text" class="form-control" id="customer-first-name"
+                   name="ccname" placeholder="###">
+        </div>
+        {{--Address 1--}}
+        <div class="form-group col-xs-12">
+            <label for="ccaddress1">Billing Address 1</label>
+            <input type="text" class="form-control" id="customer-address1"
+                   name="ccaddress1">
+        </div>
+        {{--Address 2--}}
+        <div class="form-group col-xs-12">
+            <label for="ccaddress2">Billing Address 2</label>
+            <input type="text" class="form-control" id="customer-address2"
+                   name="ccaddress2">
+        </div>
+        {{--City--}}
+        <div class="form-group col-sm-6">
+            <label for="cccity">City</label>
+            <input type="text" class="form-control"
+                   id="cccity" name="cccity">
+        </div>
+        {{--State--}}
+        <div class="form-group col-xs-6 col-sm-3">
+            <label for="ccstate">State</label>
+            <input type="text" class="form-control state-autocomplete"
+                   id="ccstate" name="ccstate">
+        </div>
+        {{--Zip--}}
+        <p><div class="form-group col-xs-6 col-sm-3">
+            <label for="cczip">Zip</label>
+            <input type="text" class="form-control" id="customer-zip"
+                   name="cczip">
+        </div>
       </div>
       <div class="modal-footer">
         <span class="pull-left">
