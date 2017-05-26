742935fd32aa7764fc17e83cbeca171606cb93fa
Arnold Koh <kohded@users.noreply.github.com>
2017-05-26 03:06:02 -0700

    Update CustomerSeeder phone with 206 prefix, was exceeding int size for dev server

--- database/seeds/CustomerSeeder.php
+++ database/seeds/CustomerSeeder.php
@@ -21,7 +21,7 @@ class CustomerSeeder extends Seeder
                 'city'       => $faker->city,
                 'state'      => $faker->stateAbbr,
                 'zip'        => substr($faker->postcode, 0, 5),
-                'phone'      => $faker->numberBetween($min = 1000000000, $max = 9999999999),
+                'phone'      => 206 . $faker->numberBetween($min = 1000000, $max = 9999999),
                 'email'      => 'ricardoparts@ricardobeverlyhills.com'
             ]);
         }
